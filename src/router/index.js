3925e06d3c66e5915ed9fabe40e012e208550f34
AntLouiz <luizrodrigo46@hotmail.com>
2020-02-13 14:58:48 -0300

    router refactored to add the new routes and protect the resource component

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,61 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import store from "../store";
+import TheLogin from "../components/auth/TheLogin";
+import TheRegister from "../components/auth/TheRegister";
+import Resources from "../components/resources/Resources";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: TheLogin
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: TheRegister
+  },
+  {
+    path: "/resources",
+    name: "Resources",
+    component: Resources,
+    meta: {
+      requiresAuth: true
+    }
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters['auth/isLoggedIn']) {
+      next();
+      return;
+    }
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+export default router;
